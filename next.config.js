1be3189cd03416d8eabee956d32a6deb5cfd074d
smallgag <smallgag@gmail.com>
2023-03-14 15:58:40 +0800

    1.修改 next config js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         // https://res.cloudinary.com/smallga/image/upload/v1543223643/image/%E5%9C%9F%E9%AD%A0%E9%AD%9A%E5%88%87%E7%89%87.jpg
-        protocol: 'https:',
+        protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '/smallga/image/upload/v1543223643/image/**.jpg',
