b22f1d7bce44d9c4ea39b8e259719bf69d77cf41
smallgag <smallgag@gmail.com>
2023-03-15 00:34:13 +0800

    1. 修改 ＡＰＩdomain

--- src/server/product.ts
+++ src/server/product.ts
@@ -1,7 +1,7 @@
 import { Product } from '@/interface/product.interface'
 import axios from 'axios'
 
-const urlProfix = 'http://localhost:3000/'
+const urlProfix = 'https://next-shop-page.vercel.app/'
 
 export async function getProduct(id: string): Promise<Product> {
   const { data: response } = await axios(`${urlProfix}/api/product/${id}`)
